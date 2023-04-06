8ae927357e371c8ab04289de01053b8c6a0c167d
Alexey <asaleseksesey12345@gmail.com>
2023-04-06 14:03:44 +0300

    Solution for FinalProject added

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+    string[] arr = { "Hello", "2", "world", ":-)", "Hi", "351" };
+    Console.WriteLine(String.Join(" ", searchElemensOfArray(arr, 3)));
+}
+
+string[] searchElemensOfArray(string[] array, int n)
+{
+    int newArrayLenght = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n) { newArrayLenght++; }
+    }
+    string[] newArray = new string[newArrayLenght];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            newArray[j++] = array[i];
+        }
+    }
+    return newArray;
+}
+Main();
